9cfa4caff55d7b96558c955b45fa31524b0ff157
gujiawei <1594485894@qq.com>
2021-12-27 22:43:06 +0800

    jd

--- js-study/Day14/05-京东放大镜效果/js/detail.js
+++ js-study/Day14/05-京东放大镜效果/js/detail.js
@@ -1,3 +1,25 @@
 let previewImg = document.querySelector('.preview_img');
 let mask = document.querySelector('.mask');
 let bigImg = document.querySelector('.big');
+
+previewImg.addEventListener('mousemove',function (e){
+    mask.style.display = 'block';
+    bigImg.style.display = 'block';
+    //获取鼠标距离小图盒子左侧和上方的位置
+    let mouseX = e.pageX - this.offsetLeft;
+    let mouseY = e.pageY - this.offsetTop;
+    if (mouseX >= mask.offsetWidth / 2 && mouseX < this.offsetWidth - mask.offsetWidth / 2){
+        //黄色选框跟随鼠标x轴移动
+        mask.style.left = mouseX - mask.offsetWidth / 2 + 'px';
+        bigImg.firstElementChild.style.left = -(mouseX - mask.offsetWidth / 2) * 2 + 'px';
+    }
+    if (mouseY >= mask.offsetHeight / 2 && mouseY < this.offsetHeight - mask.offsetHeight / 2){
+        //黄色选框跟随鼠标y轴移动
+        mask.style.top = mouseY - mask.offsetHeight / 2 + 'px';
+        bigImg.firstElementChild.style.top = -(mouseY - mask.offsetHeight / 2) * 2 + 'px';
+    }
+})
+previewImg.addEventListener('mouseout',function (){
+    mask.style.display = 'none';
+    bigImg.style.display = 'none';
+})
