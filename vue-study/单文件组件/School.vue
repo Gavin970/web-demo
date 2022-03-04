b8d36510ea0c5a3a63491f6a3aa3f3b2bfda243b
gujiawei <gujiawei@xiaoruitech.onaliyun.com>
2022-03-04 17:10:58 +0800

    vue

---
+++ vue-study/单文件组件/School.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <!-- 组件的结构 -->
+    <h2 class="demo">学校名称: {{name}}</h2>
+    <h2 class="demo">学校地址: {{address}}</h2>
+    <button @click="showVc">点我获取组件实例对象</button>
+  </div>
+</template>
+
+<script>
+    // 组件交互相关的代码
+    export default {
+        name:'School',
+        data(){
+            return {
+                name: '上政',
+                address: '上海'
+            }
+        },
+        methods:{
+            showVc(){
+                console.log(this); 
+            }
+        }
+    }
+</script>
+
+<style>
+    /* 组件的样式 */
+    .demo{
+        color: orange;
+    }
+</style>
---
+++ vue-study/单文件组件/Student.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <!-- 组件的结构 -->
+    <h2 class="demo">姓名: {{name}}</h2>
+    <h2 class="demo">年龄: {{age}}</h2>
+  </div>
+</template>
+
+<script>
+    // 组件交互相关的代码
+    export default {
+        name:'Student',
+        data(){
+            return {
+                name: '玛卡巴卡',
+                age: 16
+            }
+        },
+        methods:{
+            
+        }
+    }
+</script>
+
+<style>
+    /* 组件的样式 */
+    .demo{
+        color: skyblue;
+    }
+</style>
