dcd9691acf3c7bde33c79042f2d94e7c0c9f88ff
gujiawei <gujiawei@xiaoruitech.onaliyun.com>
2022-07-29 14:04:40 +0800

    1

--- vue3_demo/src/App.vue
+++ vue3_demo/src/App.vue
@@ -1,27 +1,16 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+  <div></div>
 </template>
 
 <script lang="ts">
-import { Options, Vue } from 'vue-class-component';
-import HelloWorld from './components/HelloWorld.vue';
-
-@Options({
-  components: {
-    HelloWorld,
+export default {
+  name: 'App',
+  setup() {
+    let num: number;
+    num = 1;
+    // console.log(num);
   },
-})
-export default class App extends Vue {}
+};
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+<style></style>
